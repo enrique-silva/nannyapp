9fcd1aee0fb430157c834c54787dcbb68831dd1f
enrique <enrique.r.silva@gmail.com>
2017-04-30 14:42:10 -0700

    My first stab at the DB model

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 
-from .models import Question
+from .models import User
 
-admin.site.register(Question)
+admin.site.register(User)
 
