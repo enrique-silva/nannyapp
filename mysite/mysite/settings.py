408b7546b7cbea6ce30dc1270034fff967579905
enrique <enrique@remitly.com>
2017-06-05 23:53:14 -0700

    changes to settings.py to add django-filters to installed apps

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mathfilters'
+    'mathfilters',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
